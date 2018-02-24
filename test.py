cf8fad5c61d0c072bb3d6905f473c18293a202df
illWill <wrj2@eris1n2.research.partners.org>
2018-02-23 21:03:45 -0500

    Updates to packages for use on ERISone

--- test.py
+++ test.py
@@ -20,24 +20,6 @@ imp.reload(blatTools)
 imp.reload(alignment)
 imp.reload(cpp_function_wrappers)
 
-
-reads = readTools.loadNanoporeFastq('RAW.fastq')
-adapterFasta = readTools.loadFasta(r'/Users/wjeck/Dropbox (Partners HealthCare)/MGH Nanopore/NanoporePythonScripts/NanoporeBarcodes.fa')
-adapters = {read.name: read.sequence for read in adapterFasta.readList}
-adapters.update ( {'~' + read.name : alignment.revcomp(read.sequence) for read in adapterFasta.readList} )
-
-adapterLead = 'GTGACTGGAGTTCAGACGTGTGCTCTTCCGATCT'
-p5 = 'AATGATACGGCGACCACCGAGATCTACAC'
-p7 = 'CAAGCAGAAGACGGCATACGAGAT'
-
-p7withBarcode = 'CAAGCAGAAGACGGCATACGAGAT TCGCCTTA             GTGACTGGAGTTCAGACGTGTGCTCTTCCGATCT' #not used, but for reference
-p5withBarcode = 'AATGATACGGCGACCACCGAGATCTACAC TAGATCGCNNWNNWNN ACACTCTTTCCCTACACGACGCTCTTCCGATCT' #not used, but for reference
-
-p7Lead = 'GTGACTGGAGTTCAGACGTGTGCTCTTCCGATCT'
-p5Lead = 'ACACTCTTTCCCTACACGACGCTCTTCCGATCT'
-
-adapters.update({'p5':p5, 'p7':p7, 'p5Lead':p5Lead,'~p5':alignment.revcomp(p5), '~p7':alignment.revcomp(p7), '~p5Lead':alignment.revcomp(p5Lead)})
-
 def printRed(text):
     RED   = "\033[1;31m"  
     BLUE  = "\033[1;34m"
@@ -85,82 +67,79 @@ def readSplitAdapter(seq,adapter):
     align = max(alignf,alignr)
     return readSplitAlignment(seq,align)
 
-def recursiveReadSplit(read):
-    '''Takes in read sequence and recursively identifies adapter matches and adds HTML style tags'''
-    # p5align = readSplitAdapter(read, p5)
-    # if(p5align[0] > 75):
-    #     return recursiveReadSplit(p5align[1]) + printGreen('<p5>' + p5align[2] + '</p5>') + recursiveReadSplit(p5align[3]) 
-    # p7align = readSplitAdapter(read, p7)
-    # if(p7align[0] > 75):
-    #     return recursiveReadSplit(p7align[1]) + printGreen('<p7>' + p7align[2] + '</p7>') + recursiveReadSplit(p7align[3])
-    adaptersAlign = bestAdapterAlign(read,adapters)
-    adaptersSplit = readSplitAlignment(read,adaptersAlign[0])
-    if(adaptersSplit[0] > 75):
-        prefix = recursiveReadSplit(adaptersSplit[1]) 
-        match = '<' + adaptersAlign[2] + '>' + adaptersSplit[2] + '</' + adaptersAlign[2] + '>'
-        suffix = recursiveReadSplit(adaptersSplit[3]) 
-        return prefix + match + suffix
-    p7LeadAlign = readSplitAdapter(read, p7Lead)
-    if(p7LeadAlign[0] > 75):
-        return recursiveReadSplit(p7LeadAlign[1]) + '<p7Lead>' + p7LeadAlign[2] + '</p7Lead>' + recursiveReadSplit(p7LeadAlign[3])
-    return read
-
 def printSegmentedRead(readText):
     '''Takes a segmented read with tags and prints it out to highlight adapters'''
     RED   = "\033[1;31m"
     RESET = "\033[0;0m"
     readText = re.sub(r'(<[^/])',RED + r'\1',readText)
     readText = re.sub(r'(</[^>]+>)', r'\1' + RESET,readText)
-    print(readText)
+    print(readText + "\n")
 
-coded = {}
-total = 0
-for read in reads.readList:
-    read_split = recursiveReadSplit(read.sequence)
-    #printSegmentedRead( read_split )
-    find_forward = re.finditer(r'</(?P<barcode>BC\d\d)>(?P<sequence>[ACTG]+)<~p5Lead>',read_split)
-    find_forward2 = re.finditer(r'</(?P<barcode>BC\d\d)>(?P<sequence>[ACTG]+)$',read_split)
-    find_reverse = re.finditer(r'</p5Lead>(?P<sequence>[ACTG]+)<~(?P<barcode>BC\d\d)>',read_split)
-    find_reverse2 = re.finditer(r'^(?P<sequence>[ACTG]+)<~(?P<barcode>BC\d\d)>',read_split)
-    for (i,match) in enumerate(itertools.chain(find_forward, find_forward2, find_reverse, find_reverse2)):
-        total+=1
-        if total%1000 == 0:
-            print(total)
-        if match.group('barcode') in coded:
-            coded[match.group('barcode')].append( (read.name + '_FRAGMENT' + str(i), match.group('sequence')))
-        else:
-            coded[match.group('barcode')] = [ (read.name + '_FRAGMENT' + str(i), match.group('sequence')) ]
-
-for a in coded.keys():
-    f = open(a + '.fasta','w')
-    for (name,sequence) in coded[a]:
-        junk = f.write('>' + name + "\n" + sequence + "\n")
-    f.close()
-
-
-
-
-#     forward = cpp_function_wrappers.adapter_alignment_simple(read.sequence[0:300], adapter)
-#     reverse = cpp_function_wrappers.adapter_alignment_simple(read.sequence[-300:], alignment.revcomp(adapter))
-#     if (max(forward,reverse)[0] < 70):
-#         continue
-#     test = '' 
-#     revflag = 0
-#     index = -1
-#     if (reverse > forward):
-#         test = alignment.revcomp(read.sequence[-350:])
-#         revflag = 1
-#     else:
-#         test = read.sequence[0:350]
-#     result = bestAdapterAlign(test,adapters)
-#     if (result is None):
-#         continue
-# 
-# len(aligns)
-# 
-# testlet = [a[3] for a in aligns]
-# from collections import Counter
-# Counter(testlet)
+def parseReadFile(fn,adapterFile):
+    prefix = ".".join(fn.split(".")[:-1])
+    reads = readTools.loadNanoporeFastq(fn)
+    
+    adapterFasta = readTools.loadFasta(adapterFile)
+    adapters = {read.name: read.sequence for read in adapterFasta.readList}
+    adapters.update ( {'~' + read.name : alignment.revcomp(read.sequence) for read in adapterFasta.readList} )
+    
+    adapterLead = 'GTGACTGGAGTTCAGACGTGTGCTCTTCCGATCT'
+    p5 = 'AATGATACGGCGACCACCGAGATCTACAC'
+    p7 = 'CAAGCAGAAGACGGCATACGAGAT'
+    
+    p7withBarcode = 'CAAGCAGAAGACGGCATACGAGAT TCGCCTTA             GTGACTGGAGTTCAGACGTGTGCTCTTCCGATCT' #not used, but for reference
+    p5withBarcode = 'AATGATACGGCGACCACCGAGATCTACAC TAGATCGCNNWNNWNN ACACTCTTTCCCTACACGACGCTCTTCCGATCT' #not used, but for reference
+    
+    p7Lead = 'GTGACTGGAGTTCAGACGTGTGCTCTTCCGATCT'
+    p5Lead = 'ACACTCTTTCCCTACACGACGCTCTTCCGATCT'
+    
+    adapters.update({'p5':p5, 'p7':p7, 'p5Lead':p5Lead,'~p5':alignment.revcomp(p5), '~p7':alignment.revcomp(p7), '~p5Lead':alignment.revcomp(p5Lead)})
+    
+    def recursiveReadSplit(read):
+      '''Takes in read sequence and recursively identifies adapter matches and adds HTML style tags'''
+      # p5align = readSplitAdapter(read, p5)
+      # if(p5align[0] > 75):
+      #     return recursiveReadSplit(p5align[1]) + printGreen('<p5>' + p5align[2] + '</p5>') + recursiveReadSplit(p5align[3]) 
+      # p7align = readSplitAdapter(read, p7)
+      # if(p7align[0] > 75):
+      #     return recursiveReadSplit(p7align[1]) + printGreen('<p7>' + p7align[2] + '</p7>') + recursiveReadSplit(p7align[3])
+      adaptersAlign = bestAdapterAlign(read,adapters)
+      adaptersSplit = readSplitAlignment(read,adaptersAlign[0])
+      if(adaptersSplit[0] > 75):
+          prefix = recursiveReadSplit(adaptersSplit[1]) 
+          match = '<' + adaptersAlign[2] + '>' + adaptersSplit[2] + '</' + adaptersAlign[2] + '>'
+          suffix = recursiveReadSplit(adaptersSplit[3]) 
+          return prefix + match + suffix
+      p7LeadAlign = readSplitAdapter(read, p7Lead)
+      if(p7LeadAlign[0] > 75):
+          return recursiveReadSplit(p7LeadAlign[1]) + '<p7Lead>' + p7LeadAlign[2] + '</p7Lead>' + recursiveReadSplit(p7LeadAlign[3])
+      return read
+    
+    allParsed = []
+    coded = {}
+    total = 0
+    for read in reads.readList:
+        read_split = recursiveReadSplit(read.sequence)
+        allParsed.append(read_split)
+        #printSegmentedRead( read_split )
+        find_forward = re.finditer(r'</(?P<barcode>BC\d\d)>(?P<sequence>[ACTG]+)<~p5Lead>',read_split)
+        find_forward2 = re.finditer(r'</(?P<barcode>BC\d\d)>(?P<sequence>[ACTG]+)$',read_split)
+        find_double = re.finditer(r'</(?P<barcode>BC\d\d)>(?P<sequence>[ACTG]+)<~(?P=barcode)>',read_split)
+        find_reverse = re.finditer(r'</p5Lead>(?P<sequence>[ACTG]+)<~(?P<barcode>BC\d\d)>',read_split)
+        find_reverse2 = re.finditer(r'^(?P<sequence>[ACTG]+)<~(?P<barcode>BC\d\d)>',read_split)
+        for (i,match) in enumerate(itertools.chain(find_forward, find_forward2, find_double, find_reverse, find_reverse2)):
+            total+=1
+            if total%1000 == 0:
+                print(total)
+            if match.group('barcode') in coded:
+                coded[match.group('barcode')].append( (read.name + '_FRAGMENT' + str(i), match.group('sequence')))
+            else:
+                coded[match.group('barcode')] = [ (read.name + '_FRAGMENT' + str(i), match.group('sequence')) ]
+    for a in coded.keys():
+        f = open(prefix + '.' + a + '.fasta','w')
+        for (name,sequence) in coded[a]:
+            junk = f.write('>' + name + "\n" + sequence + "\n")
+        f.close()
 
 #x1 = [sorted(alignment) for alignment in aligns]
 #maxed  = [a[11][0] for a in x1]
@@ -168,8 +147,11 @@ for a in coded.keys():
 #n, bins, patches = plt.hist(gapped, 50, normed=1, facecolor='green', alpha=0.75)
 
 '''System for processing a coldfusion mapped sam file... preliminary'''
-def findBestFusion(fileName):
-    sam = open(fileName,'r')
+def findBestFusion(samFileName):
+    try:
+        sam = open(samFileName,'r')
+    except IOError:
+        return({})
     readHits = {}
     for line in sam:
         if line[0] == '@':
@@ -181,42 +163,37 @@ def findBestFusion(fileName):
         else:
             readHits[fields[0]] = [fields[2]]
     sam.close()
-    hits = [tuple(readHits[r]) for r in readHits.keys() if len(readHits[r]) >= 2]
+    hits = [tuple(sorted(readHits[r])) for r in readHits.keys() if len(readHits[r]) >= 2]
     hit_dict = Counter(hits)
     import operator
     hit_sorted = sorted(hit_dict.items(), key=lambda x: -x[1])
     best_hit = hit_sorted[0]
-    print(best_hit)
-    reads_on_best_hit = [r for r in readHits.keys() if len(readHits[r]) >= 2 and best_hit[0] == (readHits[r][0], readHits[r][1])]
-    return(hit_dict)
+    #print(best_hit)
+    #reads_on_best_hit = [r for r in readHits.keys() if len(readHits[r]) >= 2 and best_hit[0] == (readHits[r][0], readHits[r][1])]
+    return(hit_sorted)
 
-#BClist = ['01','02','03','04','05','06','07','08','09','10','11','12']
-BClist = ['03']
-for i in ['03']:
-    print(i)
-    junk = findBestFusion('BC' + i + '.coldfuse.sam')
+def narrowRecursiveSplitForDoubledFasta():   ####NOT CURRENTLY USED, I don't know what to do with it. 
+    coded2 = {}
+    total = 0
+    for read in reads.readList:
+        total+=1
+        if total%1000 == 0:
+            print(total)
+        read_split = recursiveReadSplit(read.sequence)
+        #printSegmentedRead( read_split )
+        find_double = re.finditer(r'</(?P<barcode>BC\d\d)>(?P<sequence>[ACTG]+)<~(?P=barcode)>',read_split)
+        for (i,match) in enumerate(find_double):
+            if match.group('barcode') in coded2:
+                coded2[match.group('barcode')].append( (read.name + '_FRAGMENT' + str(i), match.group('sequence')))
+            else:
+                coded2[match.group('barcode')] = [ (read.name + '_FRAGMENT' + str(i), match.group('sequence')) ]
+    for a in coded2.keys():
+        f = open(a + '.doubled.fasta','w')
+        for (name,sequence) in coded[a]:
+            junk = f.write('>' + name + "\n" + sequence + "\n")
+        f.close()
 
 
-# bcrOnlyFastq = readTools.loadNanoporeFastq("BCRonlySnapshot/Processing/PRELIM.fastq")
-# bcrOnlyBlat = blatTools.loadBlat("BCRonlySnapshot/Processing/PRELIM.blat")
-# 
-# ABL = blatTools.Region("chr9",133589268,133763062);
-# BCR = blatTools.Region("chr22",23522552,23660224);
-# 
-# BCRABLhits = []
-# ABLblat = []
-# BCRblat = []
-# for readBlat in bcrOnlyBlat.genQueryGroups():
-#     readBlat.cull(80)
-#     ABLhits = readBlat.targetRegionContained(ABL)
-#     BCRhits = readBlat.targetRegionContained(BCR)
-#     if ABLhits:
-#         ABLblat.append(readBlat)
-#     if BCRhits:
-#         BCRblat.append(readBlat)
-#     if (ABLhits and BCRhits):
-#         fusionHits.append(readBlat)
-# 
 def groupQuery(blatlet):
     qgroups = []
     for hit in blatlet.records:
@@ -232,14 +209,18 @@ def groupQuery(blatlet):
                 group[2].records.append(hit)
     return qgroups
 
-fusionHits = []
-for readBlat in bcrOnlyBlat.genQueryGroups():
-    readBlat.cull(80)
-    if len(set(readBlat.targetList())) == 2:
-        qgroups = groupQuery(readBlat)
-        if len(qgroups) > 1:
-            fusionHits.append(readBlat)
+if len(sys.argv) > 1:
+    fileName = sys.argv[1]
+    adapterFile= r'/PHShome/wrj2/Scripts/ColdFusion/NanoporeBarcodes.fa'
+    parseReadFile(fileName,adapterFile)
+else:
+    BClist = ['01','02','03','04','05','06','07','08','09','10','11','12']
+    fnList = ['RAW.BC' + i + '.sam' for i in BClist]
+    for i in fnList:
+        print(i)
+        hits = findBestFusion(i)
+        for hit in hits:
+            if hit[1] > 2:
+                print(hit)
 
-for blatlet in fusionHits:
-    print(blatlet)
-    a=input()
+
